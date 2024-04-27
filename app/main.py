454473de0473ea87ff87ccc4cf3085e87d1068da
Spencer Malott <spencermalott16@gmail.com>
2024-04-27 12:29:47 -0700

    feat: add cors middleware and dotenv package

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
